275b6cb42978bcfec6a1c99c72877c8f1c057ca6
JAEGER <omer.kayalar0101@icloud.com>
2022-12-07 10:45:07 +0300

    beautify

--- src/utils/LogStream.js
+++ src/utils/LogStream.js
@@ -5,15 +5,13 @@ const CreateLogStream = () => {
   const FolderPath = path.join(__dirname, '../../logs');
   const FilePath = path.join(__dirname, '../../logs/api.zaxe.log');
 
-  if (!fs.existsSync(FolderPath)) {
-    fs.mkdirSync(FolderPath);
-  }
+  if (!fs.existsSync(FolderPath)) fs.mkdirSync(FolderPath);
 
-  if (fs.existsSync(path.join(FilePath))) {
+  if (fs.existsSync(path.join(FilePath)))
     fs.rmSync(FilePath, {
       force: true,
     });
-  }
+
   fs.writeFileSync(FilePath, `##### Zaxe Web API Log File #####\n\n`, {
     encoding: 'utf8',
   });
