6ab77d5e8b35c346a485bc2a55c9e23b6eba71b6
JAEGER <omer.kayalar0101@icloud.com>
2022-12-07 11:15:02 +0300

    feat: xdesktop route

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const PriceRouter = require('@/v1/routes/price.route');
+const XDesktopRoute = require('@/v1/routes/xdesktop.route.js');
 const ResellerRouter = require('@/v1/routes/reseller.route.js');
 
 const router = express.Router();
 
-router.use(PriceRouter, ResellerRouter);
+router.use(PriceRouter, ResellerRouter, XDesktopRoute);
 
 module.exports = router;
