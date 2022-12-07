95a811bab5ba9e25295ff476436fdff682a91eb9
JAEGER <omer.kayalar0101@icloud.com>
2022-12-07 10:43:58 +0300

    feat: Create log stream

---
+++ src/utils/LogStream.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+
+const CreateLogStream = () => {
+  const FolderPath = path.join(__dirname, '../../logs');
+  const FilePath = path.join(__dirname, '../../logs/api.zaxe.log');
+
+  if (!fs.existsSync(FolderPath)) {
+    fs.mkdirSync(FolderPath);
+  }
+
+  if (fs.existsSync(path.join(FilePath))) {
+    fs.rmSync(FilePath, {
+      force: true,
+    });
+  }
+  fs.writeFileSync(FilePath, `##### Zaxe Web API Log File #####\n\n`, {
+    encoding: 'utf8',
+  });
+
+  return fs.createWriteStream(FilePath, {
+    flags: 'a',
+    encoding: 'utf8',
+  });
+};
+
+module.exports = CreateLogStream;
