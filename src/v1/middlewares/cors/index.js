6edcba9186b198cc97369cc91cd0d30357bdc4a1
JAEGER <omer.kayalar0101@icloud.com>
2022-12-08 16:00:36 +0300

    feat: create cors middleware

---
+++ src/v1/middlewares/cors/index.js
@@ -0,0 +1,30 @@
+const AllowedOrigins = [
+  'https://zaxe.com',
+  'https://www.zaxe.com',
+  'https://learn.zaxe.com',
+  'https://careers.zaxe.com',
+];
+
+const AllowRequest = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+};
+
+const CustomCORS = (req, res, next) => {
+  const {
+    headers: { origin },
+  } = req;
+
+  if (!origin || !AllowedOrigins.includes(origin)) {
+    return res.status(403).send({
+      status: 'error',
+      message: 'Bzzt! Bzzt! Origin not allowed.',
+    });
+  }
+
+  return AllowRequest(req, res, next);
+};
+
+module.exports = CustomCORS;
