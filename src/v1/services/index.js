fd74950df1509989f0b5966f6fb8ed36beea89ea
JAEGER <omer.kayalar0101@icloud.com>
2022-12-08 15:59:35 +0300

    rev: exporting and names

---
+++ src/v1/services/index.js
@@ -0,0 +1,9 @@
+const PriceService = require('./price.service');
+const ResellerServiec = require('./reseller.service');
+const XDesktopService = require('./xdesktop.service');
+
+module.exports = {
+  PriceService,
+  ResellerServiec,
+  XDesktopService,
+};
