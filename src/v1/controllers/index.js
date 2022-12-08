baa3cb13ac495b899002c983b0146ec486b30f43
JAEGER <omer.kayalar0101@icloud.com>
2022-12-08 16:01:02 +0300

    rev: export controllers

---
+++ src/v1/controllers/index.js
@@ -0,0 +1,13 @@
+const FormsController = require('./forms.controller');
+const PriceController = require('./price.controller');
+const ResellerController = require('./reseller.controller');
+const XDesktopController = require('./xdesktop.controller');
+
+const Controllers = {
+  FormsController,
+  PriceController,
+  ResellerController,
+  XDesktopController,
+};
+
+module.exports = Controllers;
