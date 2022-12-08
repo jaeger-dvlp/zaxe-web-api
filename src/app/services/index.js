52bf32b8e903d304e9de45588d62ba9f7f8b8a2a
JAEGER <omer.kayalar0101@icloud.com>
2022-12-08 16:39:09 +0300

    fix:typo

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,9 +1,9 @@
 const PriceService = require('./price.service');
-const ResellerServiec = require('./reseller.service');
+const ResellerService = require('./reseller.service');
 const XDesktopService = require('./xdesktop.service');
 
 module.exports = {
   PriceService,
-  ResellerServiec,
+  ResellerService,
   XDesktopService,
 };
