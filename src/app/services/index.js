22e9043ef69833e2d24a2534f5f4249f0ca12935
JAEGER <omer.kayalar0101@icloud.com>
2022-12-12 11:28:10 +0300

    feat: export form service

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,8 +1,10 @@
+const FormService = require('./form.service');
 const PriceService = require('./price.service');
 const ResellerService = require('./reseller.service');
 const XDesktopService = require('./xdesktop.service');
 
 module.exports = {
+  FormService,
   PriceService,
   ResellerService,
   XDesktopService,
