c39ba99cecd0e35cfcf7e4e03a649481949b6d38
JAEGER <omer.kayalar0101@icloud.com>
2022-12-12 16:38:29 +0300

    feat: create kb feedback controller

--- src/app/controllers/forms.controller.js
+++ src/app/controllers/forms.controller.js
@@ -92,6 +92,29 @@ const NewPositiveFeedback = async (req, res) => {
   }
 };
 
+const NewFeedback = async (req, res) => {
+  try {
+    const { body } = req;
+    const response = await FormService.knowledgeBase.NewFeedback(body);
+
+    if (response?.status === 'sent') {
+      return res.status(200).send({
+        status: 'success',
+        message: 'Feedback has been sent successfully.',
+      });
+    }
+    return res.status(500).send({
+      status: 'error',
+      message: 'Something went wrong, please try again later.',
+    });
+  } catch (error) {
+    return res.status(error?.status || 500).send({
+      status: 'error',
+      message: error?.message || error,
+    });
+  }
+};
+
 module.exports = {
   main: {
     NewContactRequest,
@@ -100,5 +123,6 @@ module.exports = {
   },
   knowledgeBase: {
     NewPositiveFeedback,
+    NewFeedback,
   },
 };
