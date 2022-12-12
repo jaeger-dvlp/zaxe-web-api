93a6f20c1321e0be8a1b4b6ba6f3e30a834475cb
JAEGER <omer.kayalar0101@icloud.com>
2022-12-12 15:13:13 +0300

    feat: only allow defined fields

--- src/app/middlewares/validator/index.js
+++ src/app/middlewares/validator/index.js
@@ -1,11 +1,20 @@
-const { validationResult } = require('express-validator');
+const { validationResult, matchedData } = require('express-validator');
 
 const Validator = (req, res, next) => {
   const errors = validationResult(req);
+  const data = matchedData(req);
+
   if (!errors.isEmpty()) {
     return res.status(422).send({
       status: 'error',
       errors: errors.array({ onlyFirstError: true, flatten: true }),
+    });
+  }
+
+  if (Object.keys(data).length !== Object.keys(req.body).length) {
+    return res.status(400).send({
+      status: 'error',
+      message: 'Bzzt! Bzzt! Invalid request body.',
     });
   }
 
