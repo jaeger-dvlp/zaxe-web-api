0cedb2fe7b36861ed9a1a64badb25949637ec0a6
JAEGER <omer.kayalar0101@icloud.com>
2022-12-13 20:35:55 +0300

    rev: handle potential errors

--- src/utils/LogStream.js
+++ src/utils/LogStream.js
@@ -1,25 +1,34 @@
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
 const CreateLogStream = () => {
-  const FolderPath = path.join(__dirname, '../../logs');
-  const FilePath = path.join(__dirname, '../../logs/api.zaxe.log');
+  try {
+    const FolderPath = path.join(__dirname, '../../logs');
+    const FilePath = path.join(__dirname, '../../logs/api.zaxe.log');
 
-  if (!fs.existsSync(FolderPath)) fs.mkdirSync(FolderPath);
+    if (!fs.existsSync(FolderPath)) fs.mkdirSync(FolderPath);
 
-  if (fs.existsSync(path.join(FilePath)))
-    fs.rmSync(FilePath, {
-      force: true,
+    if (fs.existsSync(path.join(FilePath)))
+      fs.rmSync(FilePath, {
+        force: true,
+      });
+
+    fs.writeFileSync(FilePath, `##### Zaxe Web API Log File #####\n\n`, {
+      encoding: 'utf8',
     });
 
-  fs.writeFileSync(FilePath, `##### Zaxe Web API Log File #####\n\n`, {
-    encoding: 'utf8',
-  });
-
-  return fs.createWriteStream(FilePath, {
-    flags: 'a',
-    encoding: 'utf8',
-  });
+    return fs.createWriteStream(FilePath, {
+      flags: 'a',
+      encoding: 'utf8',
+    });
+  } catch (error) {
+    return console.log(
+      `${chalk.red(
+        chalk.bold('[ zaxe-api ]')
+      )} An error occurred while connecting to SMTP Server.`
+    );
+  }
 };
 
 module.exports = CreateLogStream;
