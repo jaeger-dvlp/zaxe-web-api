39a961a13f2d72d647c2078cfd0a6e7b5193903c
JAEGER <omer.kayalar0101@icloud.com>
2022-12-14 11:18:51 +0300

    feat: create new xdesktop route

--- src/app/routes/xdesktop.route.js
+++ src/app/routes/xdesktop.route.js
@@ -1,7 +1,23 @@
 const express = require('express');
+
+const CustomCORS = require('@/app/middlewares/cors');
+const Validator = require('@/app/middlewares/validator');
 const { XDesktopController } = require('@/app/controllers');
+const RateLimiter = require('@/app/middlewares/rate-limiter');
+
+const {
+  xdesktop: { DownloadXDesktopSchema },
+} = require('@/app/schemas');
 
 const router = express.Router();
+
+router.use('/xdesktop/download', CustomCORS, RateLimiter(5));
+
+router.post(
+  '/xdesktop/download',
+  [DownloadXDesktopSchema, Validator],
+  XDesktopController.downloadXDesktop
+);
 
 router.get(
   '/xdesktop/firmware-notes',
