3283fbfc7f1982013ee64954131446082a2b366b
JAEGER <omer.kayalar0101@icloud.com>
2022-12-16 01:03:29 +0300

    test: create form tests %60

--- src/tests/unit/form.test.js
+++ src/tests/unit/form.test.js
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = 'test';
 process.env.PORT = 3010;
 
+const fs = require('fs');
+const path = require('path');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
@@ -8,6 +10,15 @@ const should = chai.should();
 const server = require('../../server');
 
 chai.use(chaiHttp);
+
+const getDemoSTL = async () => {
+  const file = fs.readFileSync(
+    path.join(__dirname, '../../data/demo.stl.base64'),
+    'utf8'
+  );
+
+  return file;
+};
 
 describe('Forms Endpoint:', () => {
   it('Should send a Contact Form', (done) => {
@@ -65,4 +76,41 @@ describe('Forms Endpoint:', () => {
         done();
       });
   });
+
+  it('Should send a Sample Request Form', async () => {
+    return getDemoSTL().then(async (file) => {
+      const requestBody = {
+        fullName: 'Web Dev Testing',
+        emailAddress: 'webdev@zaxe.com',
+        phoneNumber: '+90 537 817 4972',
+        companyName: 'Zaxe 3D Printing Technologies',
+        fullAddress: 'Yesilce, Secilmis Sk. / Kagithane / Istanbul / No 2',
+        city: 'Istanbul',
+        country: 'Turkey',
+        layerHeight: '1mm',
+        fillDensity: '30%',
+        message: 'Could you print this sample please? Thanks!',
+        file: {
+          name: 'webdevKeychain.stl',
+          type: 'base64',
+          value: file,
+        },
+      };
+
+      await chai
+        .request(server)
+        .post('/v1/forms/requestsample')
+        .set('Origin', 'https://zaxe.com')
+        .send(requestBody)
+        .then((res) => {
+          const {
+            body: { message },
+          } = res;
+          should.exist(message);
+          message.should.be.eql(
+            'Request Sample request has been sent successfully.'
+          );
+        });
+    });
+  }).timeout(25000);
 });
