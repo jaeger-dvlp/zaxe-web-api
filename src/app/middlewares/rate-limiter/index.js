0618b9dd80d752d93d2b5d01236ee7602f13b0e4
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 22:43:11 +0300

    feat: handle node env

--- src/app/middlewares/rate-limiter/index.js
+++ src/app/middlewares/rate-limiter/index.js
@@ -1,10 +1,17 @@
 const rateLimiter = require('express-rate-limit');
 
-const RateLimiter = (maxRate = 5) =>
-  rateLimiter({
+const { NODE_ENV } = process.env;
+
+const RateLimiter = (maxRate = 5) => {
+  let MaxRate = maxRate;
+  if (NODE_ENV === 'test') {
+    MaxRate = 999;
+  }
+  return rateLimiter({
     windowMs: 60 * 60 * 1000,
-    max: maxRate,
+    max: MaxRate,
     message: 'Bzzt! Bzzt! Too many requests! Please try again in an hour.',
   });
+};
 
 module.exports = RateLimiter;
