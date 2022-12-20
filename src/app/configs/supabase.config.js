855eac02aa5a79c1d1cac945f257d491a4fe248c
JAEGER <omer.kayalar0101@icloud.com>
2022-12-20 17:28:49 +0300

    feat: create supabase config

---
+++ src/app/configs/supabase.config.js
@@ -0,0 +1,7 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const { SUPABASE_URL, SUPABASE_KEY } = process.env;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+module.exports = supabase;
