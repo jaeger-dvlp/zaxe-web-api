b8b9aa857647d0b1a1d1a0e38074efbb93aac84e
JAEGER <omer.kayalar0101@icloud.com>
2022-12-20 17:29:26 +0300

    export warranty route

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const PriceRouter = require('@/app/routes/price.route');
-const XDesktopRoute = require('@/app/routes/xdesktop.route.js');
-const ResellerRouter = require('@/app/routes/reseller.route.js');
-
+const XDesktopRouter = require('@/app/routes/xdesktop.route');
+const ResellerRouter = require('@/app/routes/reseller.route');
 const FormsRouter = require('@/app/routes/forms.route.js');
+const WarrantyRouter = require('@/app/routes/warranty.route');
 
 const router = express.Router();
 
-router.use(PriceRouter, ResellerRouter, XDesktopRoute, FormsRouter);
+router.use(
+  PriceRouter,
+  ResellerRouter,
+  XDesktopRouter,
+  FormsRouter,
+  WarrantyRouter
+);
 
 module.exports = router;
