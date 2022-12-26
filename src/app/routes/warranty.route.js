6c399c59f340f4d5646b64d4fe4e082dac2db288
JAEGER <omer.kayalar0101@icloud.com>
2022-12-26 11:35:04 +0300

    feat: dew new route

--- src/app/routes/warranty.route.js
+++ src/app/routes/warranty.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const CustomCORS = require('@/app/middlewares/cors');
+const WarrantyController = require('@/app/controllers/warranty.controller');
 
 const router = express.Router();
+
+router.post('/warranty/upload', CustomCORS, WarrantyController.UploadFile);
 
 module.exports = router;
