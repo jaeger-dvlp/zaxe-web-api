428968dca33e256a7ed2551ccd6300eb46a793ef
JAEGER <omer.kayalar0101@icloud.com>
2022-12-26 11:34:18 +0300

    rm unused conf

--- src/app/configs/supabase.config.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const { SUPABASE_URL, SUPABASE_KEY } = process.env;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-module.exports = supabase;
