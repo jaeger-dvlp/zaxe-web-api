8c9cb6c4e083d0a2c681fb95a74183a3c464ff8d
JAEGER <omer.kayalar0101@icloud.com>
2022-12-28 17:18:19 +0300

    export: warranty schema

--- src/app/schemas/index.js
+++ src/app/schemas/index.js
@@ -2,10 +2,13 @@ const { main, knowledgeBase, careers, xdesktop } = require('./form.schema');
 
 const { MailSchemas } = require('./mail.schema');
 
+const WarrantySchema = require('./warranty.schema');
+
 module.exports = {
   MailSchemas,
   main,
   knowledgeBase,
   careers,
   xdesktop,
+  WarrantySchema,
 };
